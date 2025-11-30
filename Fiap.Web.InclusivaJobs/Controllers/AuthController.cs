8d00596aa037d43473940c686dab857cfbcedb37
Lucas Pereira <lucaspemanuel@gmail.com>
2025-11-30 15:46:21 -0300

    Adicionar arquivos de projeto.

---
+++ Fiap.Web.InclusivaJobs/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿// Controllers/AuthController.cs
+using Microsoft.AspNetCore.Mvc;
+using InclusivaJobs.Application.ViewModels;
+
+namespace InclusivaJobs.API.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class AuthController : ControllerBase
+	{
+		public AuthController()
+		{
+		}
+
+		[HttpPost("login")]
+		public async Task<ActionResult<dynamic>> Login([FromBody] LoginViewModel loginViewModel)
+		{
+			// Implementação temporária - vamos melhorar depois
+			if (loginViewModel.Email == "admin@inclusivajobs.com" && loginViewModel.Senha == "123456")
+			{
+				return Ok(new
+				{
+					token = "token_temporario",
+					tipo = "Bearer",
+					expiraEm = DateTime.Now.AddHours(8)
+				});
+			}
+
+			return Unauthorized(new { message = "Email ou senha inválidos" });
+		}
+	}
+}
---
+++ Fiap.Web.InclusivaJobs/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fiap.Web.InclusivaJobs.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
